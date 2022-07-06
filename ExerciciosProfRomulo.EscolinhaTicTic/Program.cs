c5fe323267986582725e68cf69a4475ade5e15a2
cleverlmf <clf.nameless@gmail.com>
2022-07-06 11:46:24 -0300

    first commit

---
+++ ExerciciosProfRomulo.EscolinhaTicTic/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosProfRomulo.EscolinhaTicTic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Exercício 4 - Crie um programa para a escolinha Tic Tic
+            calcular 3 notas de um aluno.
+            Entrar com o nome do aluno e as três notas
+            Mostrar a soma das notas e a média.*/
+
+            string nomeAluno, result;
+            double num1, num2, num3;
+            double sum;
+            double avg;
+
+
+            Console.WriteLine("Digite o nome do Aluno:");
+            nomeAluno = Console.ReadLine();
+            Console.WriteLine("Digite as notas do aluno para obter a soma e a média:");
+            Console.WriteLine("Nota 1:");
+            num1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Nota 2:");
+            num2 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Nota 3:");
+            num3 = double.Parse(Console.ReadLine());
+            sum = num1 + num2 + num3;
+            avg = sum / 3;
+            //result = avg >= 7 ? "Aprovado" : "Reprovado"; 
+            if (avg >= 7)
+            {
+                result = "Aprovado";
+            }
+            else
+            {
+                result = "Reprovado";
+            }
+            //string avrs = Convert.ToString(avg);
+            //string avrl = Convert.ToString(Convert.ToInt64(avg));
+            //avrs.Substring(0, ( avrl.Length + 3 ))
+            //Console.WriteLine($"{avrl.Length} \n {avrs.Length} {avg.ToString()}");
+            Console.WriteLine($"O aluno {nomeAluno} com soma das notas {num1} + {num2} + {num3} " +
+                $"é = {sum} e sua média é = {avg.ToString().Substring(0,(Convert.ToInt64(avg).ToString().Length) + 3 )}" +
+                $", sendo assim ele foi {result}");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ExerciciosProfRomulo.MatematicaBasica/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosProfRomulo.MatematicaBasica
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Exercício 3- No projeto MatematicaBasica crie um 
+            Programa que calcule a soma de dois números inteiros.
+            Considere que o usuário vai digitar esses dois números.*/
+
+            int num1, num2;
+            int sum;
+
+            Console.WriteLine("Calcule a soma de dois numeros:");
+            Console.WriteLine("Digite o primeiro numero");
+            num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o segundo numero");
+            num2 = int.Parse(Console.ReadLine());
+            sum = num1 + num2;
+            Console.WriteLine($"O resultado da soma entre {num1} + {num2} é = {sum}");
+            Console.ReadKey();
+
+        }
+    }
+}
